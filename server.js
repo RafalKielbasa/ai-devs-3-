bfa5c32bebe124f49b15a6e3cf258e05c9ef5fad
RafalKielbasa <kielbasarafal92@gmail.com>
2025-03-03 11:31:47 +0100

    S04

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ const app = express()
 
 //Body parser
 app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/navigator', navigator)
+
+app.get('/', (req, res) => {
+  res.send('Server is running')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
