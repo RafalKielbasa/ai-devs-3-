ccee3356f9c6f49271e570156def8f5709ef9af3
RafalKielbasa <kielbasarafal92@gmail.com>
2025-03-03 11:41:30 +0100

    S04

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/navigator', navigator)
+app.use('/', navigator)
 
 app.get('/', (req, res) => {
   res.send('Server is running')
